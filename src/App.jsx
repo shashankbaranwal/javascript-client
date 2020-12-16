fee51fa755c926f48be595b07d3ab94c6ee0ea92
Shashank Baranwal <shashank.baranwal@shashank.successivetech.com>
2020-12-16 19:41:12 +0530

    feat: Completed Task 40835

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 // import { TextFieldDemo } from './pages/index';
 // import { InputDemo } from './pages/InputDemo';
-import { ChildrenDemo } from './pages';
+// import { ChildrenDemo } from './pages';
+import { Trainee } from './pages';
 
 function App() {
   return (
-    <ChildrenDemo />
+    <Trainee />
   );
 }
 
---
+++ src/pages/Trainee/Trainee.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import { AddDialog } from './components';
+
+class Trainee extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+         handleClickOpen = () => {
+           this.setState({ open: true });
+         };
+
+         handleClose = () => {
+           const { open } = this.state;
+           this.setState({ open: false });
+           return open;
+         };
+
+          handleSubmit = (data) => {
+            this.setState({
+              open: false,
+            }, () => data);
+          }
+
+          render() {
+            const { open } = this.state;
+            return (
+              <>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  onClick={this.handleClickOpen}
+                >
+                  <b>ADD TRAINEE</b>
+                </Button>
+                <AddDialog
+                  open={open}
+                  onClose={this.handleClose}
+                  onSubmit={() => this.handleSubmit}
+                />
+              </>
+            );
+          }
+}
+export default Trainee;
