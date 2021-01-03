d0fd4c7f4a176586589acb07c34efc6eb9330a8f
Shashank Baranwal <shashank.baranwal@shashank.successivetech.com>
2021-01-03 12:07:53 +0530

    feat: Deleted Handler or schema files and updated AddDialog file 40835

--- src/pages/Trainee/Trainee.jsx
+++ src/pages/Trainee/Trainee.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
 import React from 'react';
-import { Button } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import { AddDialog } from './components';
 
 class Trainee extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       open: false,
     };
@@ -16,32 +17,25 @@ class Trainee extends React.Component {
   };
 
   handleClose = () => {
-    const { open } = this.state;
     this.setState({ open: false });
-    return open;
   };
 
   handleSubmit = (data) => {
-    this.setState({
-      open: false,
-    }, () => data);
+    this.setState({ open: false });
+    console.log(data);
   }
 
   render() {
     const { open } = this.state;
     return (
       <>
-        <Button
-          variant="outlined"
-          color="primary"
-          onClick={this.handleClickOpen}
-        >
-          <b>ADD TRAINEE</b>
+        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+          Add Trainee
         </Button>
         <AddDialog
           open={open}
           onClose={this.handleClose}
-          onSubmit={() => this.handleSubmit}
+          onSubmit={this.handleSubmit}
         />
       </>
     );
