2178e8d27b932f8ec5fc1a4f2dfa22722eebda38
Shashank Baranwal <shashank.baranwal@shashank.successivetech.com>
2021-01-05 11:44:43 +0530

    feat: update auth and private layout or Login file 40989

--- src/pages/NoMatch/NoMatch.jsx
+++ src/pages/NoMatch/NoMatch.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import PropType from 'prop-types';
+import {
+  withStyles, createMuiTheme, ThemeProvider, Typography,
+} from '@material-ui/core';
 
-const NotFound = () => (
-  <div>
-    <br />
-    <Typography variant="h3" align="center">Not Found!</Typography>
-    <br />
-    <Typography variant="body1" color="textSecondary" align="center">
-      Seems like the page you are looking for after does not exists
-    </Typography>
-  </div>
-);
+const styles = () => ({
+  Text: {
+    color: 'grey',
+  },
+});
 
-export default NotFound;
+const theme = createMuiTheme({
+  typography: {
+    htmlFontSize: 10,
+    fontFamily: [
+      'Arial',
+    ].join(','),
+  },
+});
+const NotFound = (props) => {
+  const { classes } = props;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Typography>
+          <div className={classes.Text} align="center">
+            <h1>
+              NOT FOUND
+            </h1>
+            <h4>
+              seems like the pages you are looking after does not exist
+            </h4>
+          </div>
+        </Typography>
+      </ThemeProvider>
+    </>
+  );
+};
+
+NotFound.propTypes = {
+  classes: PropType.objectOf(PropType.string).isRequired,
+};
+
+export default withStyles(styles)(NotFound);
